81f5eb5a7e709571ffec3484e5cbfe50da7d5eab
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-10-26 12:20:13 -0700

    Updates

---
+++ precache-manifest.1260ffdbe05fdf0f042b34228a79035c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39b8bbc34d19ce89d4fbc0534f61766",
+    "url": "/react-on-click/index.html"
+  },
+  {
+    "revision": "9ec5135ad3d7b345a88e",
+    "url": "/react-on-click/static/css/main.ef1d802d.chunk.css"
+  },
+  {
+    "revision": "1ff226b0ee5768fc225b",
+    "url": "/react-on-click/static/js/2.375596f0.chunk.js"
+  },
+  {
+    "revision": "9ec5135ad3d7b345a88e",
+    "url": "/react-on-click/static/js/main.0c520e1e.chunk.js"
+  },
+  {
+    "revision": "57a8dee02eb20f72662e",
+    "url": "/react-on-click/static/js/runtime-main.af6e0e61.js"
+  }
+]);
